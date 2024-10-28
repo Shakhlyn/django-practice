ae4aee5c95d29d7a3e99a2d2fad90524c78dc535
Shakhlyn <shakhlyn.sh.du@gmail.com>
2024-10-28 15:56:45 +0600

    feat: get a result

--- firstDjangoProject/firstApp/urls.py
+++ firstDjangoProject/firstApp/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
 
     # http://localhost:8001/first-app/about
     path('about/', views.about, name='first-app about'),
+    path("books/", views.all_books, name="books"),
+    # path("books/<int:pk>", views.book_detail, name="book_detail"),
+    path("books/<int:book_id>", views.book_detail, name="book_detail"),
+
 ]
--- firstDjangoProject/firstApp/views.py
+++ firstDjangoProject/firstApp/views.py
@@ -1,5 +1,10 @@
+import json
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from firstApp.models import BookModel
+
 
 # Create your views here.
 def home(request):
@@ -7,3 +12,17 @@ def home(request):
 
 def about(request):
     return HttpResponse("This is about page of the first app")
+
+
+def all_books(request):
+    # getting all the books
+    books = BookModel.objects.all()
+
+    # prepare the result
+    context = {"books": books}
+    return render(request, "firstApp/index.html", context)
+
+
+def book_detail(request, book_id):
+    book =get_object_or_404(BookModel, pk=book_id)
+    return render(request, "firstApp/book_detail.html", {"book": book})
